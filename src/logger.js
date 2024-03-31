b17eacebe30d5de69cfa9a424959e12ead120858
Fazlan Nazeem <fazlan.nazeem@docker.com>
2024-03-31 12:50:36 +0100

    modify log path

--- src/logger.js
+++ src/logger.js
@@ -1,6 +1,6 @@
 const SimpleNodeLogger = require("simple-node-logger"),
   opts = {
-    logFilePath: "mylogfile.log",
+    logFilePath: "logs/mylogfile.log",
     timestampFormat: "YYYY-MM-DD HH:mm:ss.SSS",
   },
   log = SimpleNodeLogger.createSimpleLogger(opts);
